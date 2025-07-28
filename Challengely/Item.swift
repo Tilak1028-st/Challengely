3edf36eb6b07191a1f5b6d91c039ee3c3fbeef38
Tilak Shakya <shakyatilak45@gmail.com>
2025-07-29 01:01:12 +0530

    Initial Commit

---
+++ Challengely/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Challengely
+//
+//  Created by Tilak Shakya on 29/07/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
